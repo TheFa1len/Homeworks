b752ea2ba0030294a0826d74642acb2723c6bb50
TheFa1len <ajiijcard@gmail.com>
2016-07-25 20:38:14 +0300

    Update Gloves.java

--- Work5/src/ClassProject/Gloves.java
+++ Work5/src/ClassProject/Gloves.java
@@ -1,13 +1,21 @@
-package ClassProject;
-public class Gloves extends Equip{
+package Module1;
+
+/**
+ * Gloves is extending class Equip and add material of equipment
+ * 
+ * @author user
+ *
+ */
+public class Gloves extends Equip {
 	String material = "Leather";
+
 	public Gloves(String name, int price, int weight) {
 		this.label = name;
 		this.price = price;
 		this.weight = weight;
 	}
-	public void SetMaterial(String material){
-	this.material = material;	
+
+	public void setMaterial(String material) {
+		this.material = material;
 	}
 }
-	
