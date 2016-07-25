c61b4cedf7b9020eb9057f077d917ae09f17c276
TheFa1len <ajiijcard@gmail.com>
2016-07-25 20:38:29 +0300

    Update Helmet.java

--- Work5/src/ClassProject/Helmet.java
+++ Work5/src/ClassProject/Helmet.java
@@ -1,12 +1,21 @@
-package ClassProject;
+package Module1;
+
+/**
+ * Helmet is extending class Equip and add boolean type of visor existing
+ * 
+ * @author user
+ *
+ */
 public class Helmet extends Equip {
 	boolean visor = true;
-	public Helmet (String name, int price, int weight){
+
+	public Helmet(String name, int price, int weight) {
 		this.label = name;
 		this.price = price;
 		this.weight = weight;
 	}
-	public void SetVisor (boolean status){
+
+	public void setVisor(boolean status) {
 		visor = status;
 	}
 }
